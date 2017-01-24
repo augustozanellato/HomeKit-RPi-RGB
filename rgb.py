a3ccb84cb9e31a4cd2381df216d22697d601554d
Augusto Zanellato <augusto.zanellato@gmail.com>
2017-01-24 17:39:52 +0100

    finished ledutils and ported main

--- rgb.py
+++ rgb.py
@@ -1,74 +1,44 @@
 #!/usr/bin/env python2
 
 from flask import Flask, request
-from struct import unpack, pack
 import ledutils
 app = Flask(__name__)
 
 app.config['CACHE_TYPE'] = "null"
 
-LEDStrip = ledutils.RGBStrip(10, 9, 11)
-
-# Array used for storing led values
-leds = {
-	'red':        0,
-	'blue':       0,
-	'green':      0,
-	'brightness': 0,
-	'status':     False
-}
-# Led pin mapping
-pins = {
-	'red':   10,
-	'green': 9,
-	'blue':  11
-}
+ledstrip = ledutils.RGBStrip(10, 9, 11)
 
 @app.route("/led/set/<color>")
 def setColor(color):
 	try:
-		leds['red'], leds['green'], leds['blue'] = unpack('BBB', color.decode('hex'))
-		ledUpdate()
+		ledstrip.setHEX(color)
 		return 'Successfully set color: {}'.format(color)
 	except:
 		return 'Invalid color: {}'.format(color)
 
 @app.route("/led/get/color")
 def getColor():
-	global leds
-	return pack('BBB', *(leds['red'], leds['green'], leds['blue'])).encode('hex')
-
-@app.route("/led/set/b/<brightness>")
-def setBrightness(brightness):
-	leds['brightness'] = brightness
-	ledUpdate()
-	return "Successfully set brightness: {}".format(leds['brightness'])
+	return ledstrip.getColor()
 
 @app.route('/led/get/brightness')
 def getBrightness():
-	return LEDStrip.getBrightness()
+	return ledstrip.getBrightness()
 
 @app.route('/led/get/status')
 def getStatus():
-	global leds
-	if leds['status']:
-		return '1'
-	else:
-		return '0'
+	return int(ledstrip.status)
 
 @app.route('/led/off')
 def ledOff():
-	global leds
-	leds['status'] = False
-	ledUpdate()
-	return 'off'
+	ledstrip.status = False
+	ledstrip.update()
+	return getStatus()
 
 @app.route('/led/on')
 def ledOn():
-	global leds
-	leds['status'] = True
-	ledUpdate()
-	return 'on'
+	ledstrip.status = True
+	ledstrip.update()
+	return getStatus()
 
 @app.route('/')
 def index():
@@ -77,16 +47,15 @@ def index():
 @app.route('/newui.html')
 def ui():
 	try:
-		leds['red'], leds['green'], leds['blue'] = unpack('BBB', str(request.args.get('color')).decode('hex'))
-		ledUpdate()
+		ledstrip.setHEX(str(request.args.get('color')))
 	except:
 		pass
 	try:
 		if request.args.get('status') == 'on':
-			leds['status'] = True
+			ledstrip.status = True
 		elif request.args.get('status') == 'off':
-			leds['status'] = False
-		ledUpdate()
+			ledstrip.status = False
+		ledstrip.update()
 	except:
 		pass
 	return app.send_static_file('newui.html')
